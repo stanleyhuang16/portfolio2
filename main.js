8ecb68cb8b2a9861a6adaa25cbc7ae9f4e78b8f9
stanleyhuang16 <iamstanleyhuang@gmail.com>
2019-12-13 00:04:28 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 function scrollToTop() {
-    document.querySelector('body').scrollIntoView();
+  document.querySelector('body').scrollIntoView();
 }
 
 function scrollAbout() {
-    document.getElementById('about').scrollIntoView();
+  document.getElementById('about').scrollIntoView();
 }
 
 function scrollProjects() {
-    document.getElementById('projects').scrollIntoView();
+  document.getElementById('projects').scrollIntoView();
 }
 
 function scrollContact() {
-    document.getElementById('contact').scrollIntoView();
+  document.getElementById('contact').scrollIntoView();
 }
 
 // When user scrolls down 100px from top of document, show arrow icon
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.querySelector('.fa-angle-up').style.visibility = 'visible';
-    } else {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.querySelector('.fa-angle-up').style.visibility = 'visible';
+  } else {
 	document.querySelector(".fa-angle-up").style.visibility = "hidden";
-    }
+  }
 }
